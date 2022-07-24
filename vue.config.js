384100ea5c0384be9ea7ffa156127e38eb45ca70
François-Pierre <francoispierrerousseau.44@gmail.com>
2022-07-24 22:12:55 +0200

    Changement du public path pour les ressources

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,5 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: 'docs',
-  publicPath: 'jsonplaceholder-app-vue-js',
+  publicPath: '/jsonplaceholder-app-vue-js/',
 });
